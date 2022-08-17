bb2d72ab6a754ff06eb68617a5040fb600b4b583
JOOHAPARK <umin110@naver.com>
2022-08-17 20:41:30 +0900

    feat: journal plant 모델 수정

--- journals/models.py
+++ journals/models.py
@@ -2,11 +2,11 @@ from django.db import models
 from plants.models import UserPlant
 
 class Diary(models.Model):
-    user_plant_id = models.ForeignKey(UserPlant, on_delete=models.CASCADE)
-    title = models.CharField(max_length = 50)
-    body = models.TextField()
-    image = models.URLField(max_length=200)
-    growing_record = models.TextField()
+    user_plant_id = models.ForeignKey(UserPlant, on_delete=models.CASCADE, null = True, blank = True )
+    title = models.CharField(max_length = 50, null = True, blank = True )
+    body = models.TextField(null = True, blank = True )
+    image = models.URLField(max_length=200, null = True, blank = True )
+    growing_record = models.TextField(null = True, blank = True )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     grow_date_info = models.DateTimeField(auto_now_add=True)
--- plants/models.py
+++ plants/models.py
@@ -9,15 +9,15 @@ class Plant(models.Model):
     image = models.URLField(max_length=200)
 
 class UserPlant(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
-    species = models.CharField(max_length=50)
-    image = models.URLField(max_length=200)
-    water = models.IntegerField(default=0)
-    water_amount = models.IntegerField(default=0)
-    repot = models.IntegerField(default=0)
-    light = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    temperature = models.CharField(max_length=7)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)       # user는 사용자 이름이며, User의 외래키
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)     # plant는 식물명이며, Plant의 외래키
+    species = models.CharField(max_length=50)                      # species는 식물의 종을 입력받는 속성
+    image = models.URLField(max_length=200, null = True, blank = True )
+    water = models.IntegerField(default=0, null = True, blank = True )
+    water_amount = models.IntegerField(default=0, null = True, blank = True )
+    repot = models.IntegerField(default=0, null = True, blank = True )
+    light = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], null = True, blank = True )
+    temperature = models.CharField(max_length=7, null = True, blank = True )
     start_date = models.DateTimeField(auto_now_add=True)
     last_watered = models.DateTimeField(auto_now_add=True)
     created_at= models.DateTimeField(auto_now_add=True)
