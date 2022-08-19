ebcfb7af46614f9e68bb84ffac3e92569f5169ab
[JOHANSAN9] <[bomulfm@naver.com]>
2022-08-19 22:45:57 +0900

    refactor: amend accounts.urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'accounts'
+
 urlpatterns = [
-    path('signup', views.signup),
-    path('login', views.login),
-    path('logout', views.logout),
+    path('signup/', views.signup),
+    path('login/', views.login),
+    path('logout/', views.logout),
+    path('post/', views.get_all_posts),
+    path('post/<int:pk>', views.get_one_post),
+    path('write/', views.post_one_post),
+    path('put/<int:pk>', views.put_one_post),
+    path('delete/<int:pk>', views.delete_one_post),
 ]
