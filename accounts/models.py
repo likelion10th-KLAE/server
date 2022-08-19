9a87a4da4d27897b124c05b99ea3ccce1934d9b5
JOOHAPARK <umin110@naver.com>
2022-08-19 18:54:09 +0900

    feat: accounts model 수정

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
+
+class User(AbstractUser):
     name = models.CharField(max_length = 20)
+    password = models.CharField(max_length=20)
     email = models.CharField(max_length = 50, unique = True)
     uuid = models.CharField(max_length = 512)
-    username = models.CharField(max_length = 20)
+    username = models.CharField(max_length = 20, unique=True)
