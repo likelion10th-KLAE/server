c4ab2ff5c5695cfeca4097683d6875d43063d134
430lyj <430lyj@gmail.com>
2022-08-19 22:21:34 +0900

    config: DB mysql 연결

--- journals/models.py
+++ journals/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from plants.models import UserPlant
 
 class Diary(models.Model):
-    user_plant_id = models.ForeignKey(UserPlant, on_delete=models.CASCADE, null = True, blank = True )
+    user_plant = models.ForeignKey(UserPlant, on_delete=models.CASCADE, null = True, blank = True )
     title = models.CharField(max_length = 50, null = True, blank = True )
     body = models.TextField(null = True, blank = True )
     image = models.URLField(max_length=200, null = True, blank = True )
@@ -10,3 +10,7 @@ class Diary(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     grow_date_info = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        managed = False
+        db_table = 'journal'
--- klae/settings.py
+++ klae/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'klae.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST_NAME'),
+        'PORT': 3306,
     }
 }
 
